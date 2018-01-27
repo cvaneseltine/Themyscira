5022b0c99743c068d9d4ce26d79eb711db90ecf6
cvaneseltine <carolyn@sibylmoon.com>
2018-01-26 20:32:33 -0500

    Adding input manager, working on puzzles

---
+++ UnityProject/Themyscira/Assets/Scripts/Puzzle/Puzzle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : ScriptableObject {
+	public Solution solution;
+	public Word[] words;
+	
+}
